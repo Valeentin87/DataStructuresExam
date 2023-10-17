a4348baff63704fcf68b085f7cc5e75af498a75a
Valeentin87 <valeentin87@list.ru>
2023-10-17 20:28:51 +0300

    реализовали метод поиска элемента в бинарном дереве без балансировки

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,32 @@
 package org.example;
 
+class BinaryTree{
+    Node root; // root-овая нода, корень бинарного дерева
 
+    class Node{   // класс узла
+        int value;  // значение ноды
+        Node left; // левый дочерний элемент
+        Node right; // правый дочерний элемент
+
+    }
+
+    public Boolean find(int value){ // метод поиска элемента в дереве
+        Node node = root;
+        while(node != null) {
+            if(node.value == value){ // если значение текущей ноды равно искомому значению
+                return true;
+            }
+            if(node.value < value){ // если значение текущей ноды меньше, чем искомое значение значит смещаемся вправо
+                node = node.right; // переходим на правую дочернюю ноду
+            }
+            else{
+                node = node.left; // иначе переходим на левую дочернюю ноду
+            }
+        }
+        return false;  // если не нашли интересующее нас искомое значение
+    }
+
+}
 
 
 public class Main {
